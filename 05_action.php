834b1736f675f64fa1359469da4ebec21cb0dc69
usumai <usumai@gmail.com>
2019-05-15 14:59:30 +1000

    auto commit

--- 05_action.php
+++ 05_action.php
@@ -24,14 +24,15 @@ if ($act=='sys_pull_master') {
 	// }
 	ini_set('max_execution_time', 0);
 	$output  = shell_exec($addr_git.' init 2>&1'); 
+	$output  = shell_exec($addr_git.' checkout working_development'); 
 	$output .= shell_exec($addr_git.' add -A'); 
 	$output .= shell_exec($addr_git.' commit -m "auto commit"'); 
 	$output .= shell_exec($addr_git.' remote add origin https://github.com/usumai/smart_public.git'); 
-	$output .= shell_exec($addr_git.' push -u origin master');
+	$output .= shell_exec($addr_git.' push -u origin working_development');
 	// $output = shell_exec('set 2>&1');  The 2>&1 makes the command get all errors
 	echo "<pre>$output</pre>";
 	
-	header("Location: index.php");
+	// header("Location: index.php");
 }elseif ($act=='sys_initialise') {
 
 	$sql = "SELECT count(*) as dbexists FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'smartdb'";
--- index.php
+++ index.php
@@ -12,7 +12,23 @@
 <?php include "02_header.php"; ?>
 
 
-test
+<!-- 
+
+always working on the working_development branch
+pushing saves to the cloud working_development
+
+when ready to publish
+we make a final push to the working_dev branch keeping them both in sync
+
+we change to the master branch
+we merge the working branch into the master branch
+
+we push the new master branch to remote
+
+
+
+
+ -->
 <?
 
 
