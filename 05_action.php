010f32348ef62fec9384ebfb9ecda2c14d2616f8
usumai <usumai@gmail.com>
2019-05-15 13:08:01 +1000

    auto commit

--- 05_action.php
+++ 05_action.php
@@ -1,13 +1,15 @@
 <?php
 $act = $_GET["act"];
 
+	$addr_git = ' "\Program Files\Git\bin\git"  ';
 //CRUD
 if ($act=='sys_pull_master') {
 	//This file updates the local software with the currently published software
-	$output  = shell_exec('git init 2>&1'); 
-	$output .= shell_exec('git clean  -d  -f .');
-	$output .= shell_exec('git reset --hard');  
-	$output .= shell_exec('git pull https://github.com/usumai/smart_public.git');
+
+	$output  = shell_exec($addr_git.' init 2>&1'); 
+	$output .= shell_exec($addr_git.' clean  -d  -f .');
+	$output .= shell_exec($addr_git.' reset --hard');  
+	$output .= shell_exec($addr_git.' pull https://github.com/usumai/smart_public.git');
 	echo "<pre>$output</pre>";
 
 	// header("Location: index.php");
@@ -17,11 +19,11 @@ if ($act=='sys_pull_master') {
 	//     echo "exec is enabled";
 	// }
 	ini_set('max_execution_time', 0);
-	$output  = shell_exec('git init 2>&1'); 
-	$output .= shell_exec('git add -A'); 
-	$output .= shell_exec('git commit -m "auto commit"'); 
-	$output .= shell_exec('git remote add origin https://github.com/usumai/smart_public.git'); 
-	$output .= shell_exec('git push -u origin master');
+	$output  = shell_exec($addr_git.' init 2>&1'); 
+	$output .= shell_exec($addr_git.' add -A'); 
+	$output .= shell_exec($addr_git.' commit -m "auto commit"'); 
+	$output .= shell_exec($addr_git.' remote add origin https://github.com/usumai/smart_public.git'); 
+	$output .= shell_exec($addr_git.' push -u origin master');
 	// $output = shell_exec('set 2>&1');  The 2>&1 makes the command get all errors
 	echo "<pre>$output</pre>";
 	
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <main role="main" class="flex-shrink-0">
 	<div class="container">
 		<h1 class="mt-5">SMART Mobile</h1>
-		<p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>main &gt; .container</code>.</p>
+		<p class="lead">New auto updating software.</p>
 		<p>Back to <a href="/docs/4.3/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
 	</div>
 </main>
