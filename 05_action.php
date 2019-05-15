656ff1fccfbcd2f9d5255d1ea15b5ec9389da9bb
usumai <usumai@gmail.com>
2019-05-15 15:52:18 +1000

    auto commit

--- 05_action.php
+++ 05_action.php
@@ -47,11 +47,11 @@ if ($act=='sys_pull_master') {
 
 	
 	
-	$output .= shell_exec($addr_git.' merge -s ours master');
+	// $output .= shell_exec($addr_git.' merge -s ours master');
 	// we change to the master branch
 	$output .= shell_exec($addr_git.' checkout master');
 	// Make the merge which should be a fast forward due to the development being based of the master and the master not being touched
-	$output .= shell_exec($addr_git.' merge working_development');
+	// $output .= shell_exec($addr_git.' merge working_development');
 
 	// we push the new master branch to remote
 	$output .= shell_exec($addr_git.' push -u origin master');
