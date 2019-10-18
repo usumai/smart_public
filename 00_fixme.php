2fe87cbd30ccedc13ae3310ee3be2445b785351c
Lucas <usumai@gmail.com>
2019-10-18 13:17:18 +1100

    auto commit

--- 00_fixme.php
+++ 00_fixme.php
@@ -16,6 +16,7 @@ mysqli_multi_query($con,$sql_save);
 
 $addr_git= ' "\Program Files\Git\bin\git"  ';
 $output  = shell_exec($addr_git.' init 2>&1'); 
+$output  = shell_exec($addr_git.' remote set-url https://github.com/usumai/110_smart.git'); 
 $output .= shell_exec($addr_git.' clean  -d  -f .');
 $output .= shell_exec($addr_git.' reset --hard');  
 $output .= shell_exec($addr_git.' pull https://github.com/usumai/110_smart.git');
